c667ba684c94a068be8cbd5cb41a11d75be94a18
Hdonge <harshal.donge@gmail.com>
2019-02-27 11:56:21 +0530

    communicating with child component

--- ng-fundamentals/src/app/app.module.ts
+++ ng-fundamentals/src/app/app.module.ts
@@ -3,9 +3,11 @@ import { NgModule } from '@angular/core';
 
 import { EventsAppComponent } from './events-app.component';
 import { EventsListComponent } from './events/events-list.component';
+import { EventThumbnailComponent } from './events/event-thumbnail.component';
 
 @NgModule({
   declarations: [
+    EventThumbnailComponent,
     EventsListComponent,
     EventsAppComponent
   ],
