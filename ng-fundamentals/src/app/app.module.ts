ab13bbd60bb05a7d87e4bddee46b20bb42c72411
Hdonge <harshal.donge@gmail.com>
2019-02-27 12:53:09 +0530

    Adding a site header

--- ng-fundamentals/src/app/app.module.ts
+++ ng-fundamentals/src/app/app.module.ts
@@ -4,9 +4,11 @@ import { NgModule } from '@angular/core';
 import { EventsAppComponent } from './events-app.component';
 import { EventsListComponent } from './events/events-list.component';
 import { EventThumbnailComponent } from './events/event-thumbnail.component';
+import { NavbarComponent } from './nav/navbar.component';
 
 @NgModule({
   declarations: [
+    NavbarComponent,
     EventThumbnailComponent,
     EventsListComponent,
     EventsAppComponent
